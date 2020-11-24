433f845605a7b0e7a81c5f22ce9b6a04918130ee
Robert Almeida <robertalmeida1597@gmail.com>
2020-11-24 13:26:45 -0300

    salvando rep

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDTMQqn-Di2Y5a7ZlphyJPAUR_JA1TcRYc",
+    authDomain: "leads-7680c.firebaseapp.com",
+    databaseURL: "https://leads-7680c.firebaseio.com",
+    projectId: "leads-7680c",
+    storageBucket: "leads-7680c.appspot.com",
+    messagingSenderId: "206183006829",
+    appId: "1:206183006829:web:d7d2c63bedb49e81f1b564",
+    measurementId: "G-7H2LKHD2Z1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
